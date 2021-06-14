2682e99de9e8a0cb3f5d8455ff5b4ff97a9f6111
Anwesh43 <anweshthecool0@gmail.com>
2021-06-15 03:04:08 +0530

    adding constants

--- sweeplinethrowgoview/src/main/java/com/example/sweeplinethrowgoview/SweepLineThrowGoView.kt
+++ sweeplinethrowgoview/src/main/java/com/example/sweeplinethrowgoview/SweepLineThrowGoView.kt
@@ -1,2 +1,27 @@
 package com.example.sweeplinethrowgoview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val deg : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
