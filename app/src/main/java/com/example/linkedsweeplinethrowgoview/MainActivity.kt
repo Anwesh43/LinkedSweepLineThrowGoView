0f9f43f1964ae8e3ed458a22d0063f9ad50a456a
Anwesh43 <anweshthecool0@gmail.com>
2021-06-15 03:35:02 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedsweeplinethrowgoview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsweeplinethrowgoview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsweeplinethrowgoview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.sweeplinethrowgoview.SweepLineThrowGoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SweepLineThrowGoView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
