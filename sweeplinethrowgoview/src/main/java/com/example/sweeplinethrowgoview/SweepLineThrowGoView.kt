7002bd2a8be58be24a038a4ed69bef4cec0db893
Anwesh43 <anweshthecool0@gmail.com>
2021-06-15 03:28:33 +0530

    adding SweepLineThrowGo class

--- sweeplinethrowgoview/src/main/java/com/example/sweeplinethrowgoview/SweepLineThrowGoView.kt
+++ sweeplinethrowgoview/src/main/java/com/example/sweeplinethrowgoview/SweepLineThrowGoView.kt
@@ -166,4 +166,27 @@ class SweepLineThrowGoView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SweepLineThrowGo(var i : Int) {
+
+        private var curr : SLTGNode = SLTGNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
